f7b9baf2b4d893e776c3111ed7f5456e72b29f0c
harryhmohney <harrison.mohney@gmail.com>
2023-09-25 07:21:02 -0700

    scaffold

---
+++ backend/index.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const morgan = require('morgan');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+mongoose.connect(process.env.DATABASE_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+app.use(express.json());
+app.use(cors());
+app.use(morgan('dev'));
+
+const strainsRouter = require('./routes/strains');
+app.use('/strains', strainsRouter);
+
+
+app.listen(process.env.PORT || 5000, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
